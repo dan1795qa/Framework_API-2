b98287040a451fe0797f7aa98772730f9c6c7022
Daniil <dan1795dan@gmail.com>
2025-12-16 20:37:19 +0300

    Acquaintance with the Faker library

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, Field
+from tools.fakers import fake
 
 
 class FileSchema(BaseModel):
@@ -16,6 +17,6 @@ class CreateFileResponseSchema(BaseModel):
 
 class CreateFileRequestSchema(BaseModel):
     """Описание структуры запроса на создание файла."""
-    filename: str
-    directory: str
+    filename: str = Field(default_factory=lambda: f"{fake.uuid4()}.png")
+    directory: str = Field(default="tests")
     upload_file: str
