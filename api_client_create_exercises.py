185bd2b20555c328984c3167774b37fc587adb7e
Daniil <dan1795dan@gmail.com>
2025-12-16 21:36:25 +0300

    Application of random data generation(HW)

--- api_client_create_exercises.py
+++ api_client_create_exercises.py
@@ -7,18 +7,11 @@ from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.fakers import fake
 
 public_users_client = get_public_users_client()
 
 # Создаем пользователя
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password="string",
-    last_name='string',
-    first_name='string',
-    middle_name='string'
-)
+create_user_request = CreateUserRequestSchema()
 create_user_response = public_users_client.create_user(create_user_request)
 
 # Инициализируем клиенты
@@ -30,21 +23,12 @@ files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
 
 # Загружаем файл
-create_file_request = CreateFileRequestSchema(
-    filename="image.png",
-    directory="courses",
-    upload_file="testdata/files/qa.png"
-)
+create_file_request = CreateFileRequestSchema(upload_file="testdata/files/qa.png")
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
 # Создаем курс
 create_course_request = CreateCourseRequestSchema(
-    title="Python",
-    maxScore=100,
-    minScore=10,
-    description="Python API course",
-    estimatedTime="2 weeks",
     previewFileId=create_file_response.file.id,
     createdByUserId=create_user_response.user.id
 )
@@ -55,13 +39,7 @@ exercise_client = get_exercises_client(authentication_user)
 
 # Создаем задание
 create_exercise_request = CreateExercisesRequestSchema(
-    title="Python basic syntax",
     courseId=create_course_response.course.id,
-    maxScore=100,
-    minScore=10,
-    orderIndex=10,
-    description="Exercise 1",
-    estimatedTime="45 minutes"
 )
 
 create_exercise_response = exercise_client.create_exercise(create_exercise_request)
