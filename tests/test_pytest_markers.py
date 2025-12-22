8b08b23f8992bddd55f3f4430dced727d6c5565f
Daniil <dan1795dan@gmail.com>
2025-12-22 12:32:21 +0300

    Working with custom markers

---
+++ tests/test_pytest_markers.py
@@ -0,0 +1,52 @@
+import pytest
+
+
+# @pytest.mark.smoke
+# def test_smoke_case():
+#     assert 1 + 1 == 2
+#
+#
+# @pytest.mark.regression
+# def test_regression_case():
+#     assert 2 + 2 == 4
+#
+#
+# @pytest.mark.fast
+# def test_fast():
+#     print("Fast tests")
+#
+#
+# @pytest.mark.slow
+# def test_slow():
+#     print("Slow tests")
+#
+#
+# @pytest.mark.smoke
+# class TestSuite:
+#     def test_case_1(self):
+#         pass
+#
+#     def test_case_2(self):
+#         pass
+#
+#
+# @pytest.mark.regression
+# class TestUserAuthentication:
+#
+#     @pytest.mark.smoke
+#     def test_login(self):
+#         pass
+#
+#     @pytest.mark.slow
+#     def test_password_reset(self):
+#         pass
+#
+#     def test_logout(self):
+#         pass
+#
+#
+# @pytest.mark.regression
+# @pytest.mark.smoke
+# @pytest.mark.slow
+# def test_password_reset(self):
+#     pass
