08af81c96e1c8e96afdb85424dd999c2f6f8dbf4
Daniil <dan1795dan@gmail.com>
2026-01-18 16:50:39 +0300

    Refactoring and preparation for writing API autotests

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -15,34 +15,32 @@ from tools.fakers import fake
 
 @pytest.mark.users
 @pytest.mark.regression
-@pytest.mark.parametrize("email", [
-    "mail.ru",
-    "gmail.com",
-    "example.com"
-])
-def test_create_user(email: str, public_users_client: PublicUsersClient):
-    request = CreateUserRequestSchema(
-        email=fake.email(domain=email)
-    )
+class TestUsers:
+    @pytest.mark.parametrize("email", [
+        "mail.ru",
+        "gmail.com",
+        "example.com"
+    ])
+    def test_create_user(self, email: str, public_users_client: PublicUsersClient):
+        request = CreateUserRequestSchema(
+            email=fake.email(domain=email)
+        )
 
-    response = public_users_client.create_user_api(request)
-    response_data = CreateUserResponseSchema.model_validate_json(response.text)
+        response = public_users_client.create_user_api(request)
+        response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
-    # Используем функцию для проверки статус-кода
-    assert_status_code(response.status_code, HTTPStatus.OK)
-    # Используем функцию для проверки ответа создания юзера
-    assert_create_user_response(request, response_data)
+        # Используем функцию для проверки статус-кода
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        # Используем функцию для проверки ответа создания юзера
+        assert_create_user_response(request, response_data)
 
-    validate_json_schema(response.json(), response_data.model_json_schema())
+        validate_json_schema(response.json(), response_data.model_json_schema())
 
+    def test_get_user_me(self, private_users_client: PrivateUsersClient, function_user: UserFixture):
+        response = private_users_client.get_user_me_api()
+        response_data = GetUserResponseSchema.model_validate_json(response.text)
 
-@pytest.mark.users
-@pytest.mark.regression
-def test_get_user_me(private_users_client: PrivateUsersClient, function_user: UserFixture):
-    response = private_users_client.get_user_me_api()
-    response_data = GetUserResponseSchema.model_validate_json(response.text)
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_user_response(response_data, function_user.response)
 
-    assert_status_code(response.status_code, HTTPStatus.OK)
-    assert_get_user_response(response_data, function_user.response)
-
-    validate_json_schema(response.json(), response_data.model_json_schema())
+        validate_json_schema(response.json(), response_data.model_json_schema())
