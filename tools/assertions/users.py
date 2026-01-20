8cf82a81f831a5265d90f29bd0b32b47732aca10
Daniil <dan1795dan@gmail.com>
2026-01-20 17:25:02 +0300

    Сorrectув errors

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -19,23 +19,29 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
 
 def assert_user(actual: UserSchema, expected: UserSchema):
     """
-    Проверяет, что ответ на текущего пользователя соответствует ожидаемому.
+    Проверяет, что фактические данные пользователя соответствует ожидаемому.
 
-    :param actual: Фактическое значение схемы.
-    :param expected: Ожидаемое значение схемы.
+    :param actual: Фактические данные пользователя.
+    :param expected: Ожидаемие данные пользователя.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    assert_equal(actual.id, expected.id, 'email')
+    assert_equal(actual.id, expected.id, 'id')
     assert_equal(actual.email, expected.email, 'email')
     assert_equal(actual.last_name, expected.last_name, 'last_name')
     assert_equal(actual.first_name, expected.first_name, 'first_name')
     assert_equal(actual.middle_name, expected.middle_name, 'middle_name')
 
 
-def assert_get_user_response(get_user_response: GetUserResponseSchema,
-                             create_user_response: CreateUserResponseSchema):
+def assert_get_user_response(
+        get_user_response: GetUserResponseSchema,
+        create_user_response: CreateUserResponseSchema
+):
     """
-    Проверяет, что пользователь из get-ответа совпадает с пользователем из create-ответа.
+    Проверяет, что ответ на получение пользователя соответствует ответу на его создание.
+
+    :param get_user_response: Ответ API при запросе данных пользователя.
+    :param create_user_response: Ответ API при создании пользователя.
+    :raises AssertionError: Если данные пользователя не совпадает.
     """
     # Берём объекты UserSchema из ответов
     actual_user = get_user_response.user
