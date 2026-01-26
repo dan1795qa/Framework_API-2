5e8b89540e2e3a53bf72f056c4cb61456f3f13da
Daniil <dan1795dan@gmail.com>
2026-01-26 16:07:39 +0300

    Practice writing negative API autotest - get file with incorrect uuid (HW)

--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -109,9 +109,31 @@ def assert_file_not_found_response(actual: InternalErrorResponseSchema):
     Функция для проверки ошибки, если файл не найден на сервере.
 
     :param actual: Фактический ответ.
-    :raises AssertionError: Если фактический ответ не соответствует ошибке "File not found"
+    :raises AssertionError: Если фактический ответ не соответствует ожидаемому.
     """
     # Ожидаемое сообщение об ошибке, если файл не найден
     expected = InternalErrorResponseSchema(details="File not found")
     # Используем ранее созданную функцию для проверки внутренней ошибки
     assert_internal_error_response(actual, expected)
+
+
+def assert_get_file_with_incorrect_file_id_response(actual: ValidationErrorResponseSchema):
+    """
+    Проверяет, что ответ на получение файла с некорректным uuid соответствует ожидаемой валидационной ошибке.
+
+    :param actual: Ответ от API с ошибкой валидации, который необходимо проверить.
+    :raises AssertionError: Если фактический ответ не соответствует ожидаемому.
+    """
+    expected = ValidationErrorResponseSchema(
+        details=[
+            ValidationErrorSchema(
+                type="uuid_parsing",  # Тип ошибки, связанной с невалидным UUID.
+                input="incorrect-file-id",  # Некорректный uuid.
+                context={"error": "invalid character: expected an optional prefix of `urn:uuid:` followed by [0-9a-fA-F-], found `i` at 1"},  # Минимальная длина строки должна быть 1 символ.
+                message="Input should be a valid UUID, invalid character: expected an optional prefix of `urn:uuid:` followed by [0-9a-fA-F-], found `i` at 1",
+                # Сообщение об ошибке.
+                location=["path", "file_id"]  # Ошибка возникает в теле запроса, поле "directory".
+            )
+        ]
+    )
+    assert_validation_error_response(actual, expected)
