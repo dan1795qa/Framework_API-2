e037da58cc9dbb4430f5a66cf26830f7f3264f2f
Daniil <dan1795dan@gmail.com>
2026-02-07 12:21:28 +0300

    Practice writing API autotest - TestExercises: test_get_exercise(HW)

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -2,16 +2,19 @@ import pytest
 from http import HTTPStatus
 
 from clients.exercises.exercises_client import ExercisesClient
-from clients.exercises.exercises_schema import CreateExercisesRequestSchema, CreateExercisesResponseSchema
+from clients.exercises.exercises_schema import CreateExercisesRequestSchema, CreateExercisesResponseSchema, \
+    GetExercisesQuerySchema, GetExerciseResponseSchema
+from fixtures.courses import CourseFixture
+from fixtures.exercises import ExercisesFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
 from tools.assertions.schema import validate_json_schema
 
 
 @pytest.mark.exercises
 @pytest.mark.regression
 class TestExercises:
-    def test_create_exercise(self, exercises_client: ExercisesClient,  function_course):
+    def test_create_exercise(self, exercises_client: ExercisesClient, function_course: CourseFixture):
         request = CreateExercisesRequestSchema(
             course_id=function_course.response.course.id
         )
@@ -20,5 +23,14 @@ class TestExercises:
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_create_exercise_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercise(self, exercises_client: ExercisesClient, function_exercise: ExercisesFixture):
+        response = exercises_client.get_exercise_api(function_exercise.response.exercise.id)
+        response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_exercise_response(response_data, function_exercise.response)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
