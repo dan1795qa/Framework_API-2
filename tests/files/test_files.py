1bd2db9650dc18e8ec9135558e1fd8a70ce31a32
Daniil <dan1795dan@gmail.com>
2026-02-27 13:13:42 +0300

    Configured autotests with pydantic_settings for 'files'(HW)

--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -16,6 +16,7 @@ from tools.assertions.files import assert_create_file_response, assert_get_file_
     assert_create_file_with_empty_directory_response, assert_get_file_with_incorrect_file_id_response
 from tools.assertions.schema import validate_json_schema
 from allure_commons.types import Severity
+from config import settings
 
 
 @pytest.mark.files
@@ -32,7 +33,7 @@ class TestFiles:
     @allure.severity(Severity.BLOCKER)
     @allure.sub_suite(AllureStory.CREATE_ENTITY)
     def test_create_file(self, files_client: FilesClient):
-        request = CreateFileRequestSchema(upload_file="./testdata/files/qa.png")
+        request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
         response = files_client.create_file_api(request)
         response_data = CreateFileResponseSchema.model_validate_json(response.text)
 
@@ -84,7 +85,7 @@ class TestFiles:
     def test_create_file_with_empty_directory(self, files_client: FilesClient):
         request = CreateFileRequestSchema(
             directory="",
-            upload_file="./testdata/files/qa.png"
+            upload_file=settings.test_data.image_png_file
         )
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
