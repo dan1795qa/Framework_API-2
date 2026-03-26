423e2d392bab5c3813d67ef47b7496a2eafcdb31
Daniil <dan1795dan@gmail.com>
2026-03-26 23:33:22 +0300

    Clean tests folder

--- tools/allure/features.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class AllureFeatures(str, Enum):
-    USERS = "Users"
-    FILES = "Files"
-    COURSES = "Courses"
-    EXERCISES = "Exercises"
-    AUTHENTICATION = "Authentication"
